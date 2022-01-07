ff982b348d1396c90c28f6a0feda44d3b516ad69
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2022-01-07 23:36:10 +0800

    Update the math

--- js/index.js
+++ js/index.js
@@ -201,21 +201,16 @@ function plant(coordinate, action){
     const points = new Array();
 
     if(action == "p1"){
-        points.push(coordinate - 11);
-        points.push(coordinate - 10);
-        points.push(coordinate - 9);
-        points.push(coordinate - 1);
-        points.push(coordinate);
-        points.push(coordinate + 1);
-        points.push(coordinate + 9);
-        points.push(coordinate + 10);
-        points.push(coordinate + 11);
-
-        for(i = 0; i < points.length; i++){
-            if (points[i] < 0 || points[i] > 99){
-                points.splice(i);
-            }
-        }
+        var offset = [[-11,-10,-9],[-1,0,1],[9,10,11]];
+        offset.forEach(o => {
+            tens = Math.floor((coordinate + o[1]) / 10)
+            o.forEach(n => {
+                var val = coordinate + n;
+                if ((Math.floor(val/10) == tens) && (val >= 0 && val < 100)) {
+                    points.push(val);
+                }
+            })
+        })
         return points;
     }
     if(action == "p2"){
@@ -239,30 +234,26 @@ function plant(coordinate, action){
         return points;
     }
     if(action == "h1"){
-        points.push(coordinate - 10);
-        points.push(coordinate - 9);
-        points.push(coordinate);
-        points.push(coordinate + 1);
-
-        for(i = 0; i < points.length; i++){
-            if (points[i] < 0 || points[i] > 99){
-                points.splice(i);
-            }
-        }
+        var offset = [[-10, -9], [0, 1]];
+        offset.forEach(o => {
+            tens = Math.floor((coordinate + o[0]) / 10)
+            o.forEach(n => {
+                var val = coordinate + n;
+                if ((Math.floor(val/10) == tens) && (val >= 0 && val < 100)) {
+                    points.push(val);
+                }
+            })
+        })
         return points;
     }
     if(action == "h2"){
-        points.push(coordinate - 3);
-        points.push(coordinate - 2);
-        points.push(coordinate - 1);
-        points.push(coordinate);
+        var offset = [0,10,20,30]
 
-        for(i = 0; i < points.length; i++){
-            if (points[i] < 0 || points[i] > 99){
-                points.splice(i);
+        offset.forEach(o => {
+            if (coordinate + o < 100) {
+                points.push(coordinate + o)
             }
-        }
-        return points;
+        })
     }
     if(action == "h3"){
         points.push(coordinate);
@@ -278,32 +269,39 @@ function plant(coordinate, action){
         return points;
     }
     if(action == "s1"){
-        points.push(coordinate - 10);
-        points.push(coordinate - 1);
-        points.push(coordinate);
-        points.push(coordinate + 1);
-        points.push(coordinate + 10);
-
-        for(i = 0; i < points.length; i++){
-            if (points[i] < 0 || points[i] > 99){
-                points.splice(i);
+        var offsetVert = [-10, 0, 10];
+        offsetVert.forEach(o => {
+            var val = coordinate + o;
+            if (val >= 0 && val < 100) {
+                points.push(val)
             }
-        }
+        })
+        var offsetHoriz = [-1, 1];
+        var tens = Math.floor((coordinate) / 10)
+        offsetHoriz.forEach(o => {
+            var val = coordinate + o;
+            if (Math.floor(val/10) == tens) {
+                points.push(val);
+            }
+        })
         return points;
+
     }
     if(action == "s2"){
-        points.push(coordinate - 11);
-        points.push(coordinate - 9);
         points.push(coordinate);
-        points.push(coordinate + 9);
-        points.push(coordinate + 11);
 
-        for(i = 0; i < points.length; i++){
-            if (points[i] < 0 || points[i] > 99){
-                points.splice(i);
-            }
-        }
+        var offset = [[-11,-9],[9,11]];
+        offset.forEach(o => {
+            tens = Math.floor((coordinate + (o[1] + o[0])/2) / 10)
+            o.forEach(n => {
+                var val = coordinate + n;
+                if ((Math.floor(val/10) == tens) && (val >= 0 && val < 100)) {
+                    points.push(val);
+                }
+            })
+        })
         return points;
+
     } else {
         points.push(coordinate);
         return points
